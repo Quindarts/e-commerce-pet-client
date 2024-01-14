19aa85439ea4e0135d6990fa3e0d64a1107bcfda
vunontay <hoangvu200202@gmail.com>
2024-01-14 12:50:54 +0700

    OurServices

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,9 +2,45 @@
 
 import logo from './svg/logo.svg'
 import widget1 from './images/widget1.jpg'
+import services1 from './svg/services1.svg'
+import services2 from './svg/services2.svg'
+import services3 from './svg/services3.svg'
+import services4 from './svg/services4.svg'
+import services5 from './svg/services5.svg'
+import services6 from './svg/services6.svg'
+import services7 from './svg/services7.svg'
+import services8 from './svg/services8.svg'
+
+import about1 from './images/about1.jpg'
+import about2 from './images/about2.jpg'
+import about5 from './images/about5.jpg'
+import about6 from './images/about6.jpg'
+import about7 from './images/about7.jpg'
+import about8 from './images/about8.jpg'
+import about9 from './images/about9.jpg'
+import about10 from './images/about10.jpg'
+import about11 from './images/about11.jpg'
+
 const images = {
     logo: logo,
     widget1: widget1,
+    services1: services1,
+    services2: services2,
+    services3: services3,
+    services4: services4,
+    services5: services5,
+    services6: services6,
+    services7: services7,
+    services8: services8,
+    about1: about1,
+    about2: about2,
+    about5: about5,
+    about6: about6,
+    about7: about7,
+    about8: about8,
+    about9: about9,
+    about10: about10,
+    about11: about11,
 }
 
 export default images
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import OurServices from '../../components/Shared/OurServices'
 
 function Home() {
     return (
         <div style={{ height: '200vh' }}>
             Home
             <Link to="component"> Go to component page</Link>
+            <OurServices />
         </div>
     )
 }
